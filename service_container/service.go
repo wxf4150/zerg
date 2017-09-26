0864e98a3e29f08560b571869665c07b80efc3b5
伍晓飞 <wxf4150@163.com>
2017-09-26 15:52:40 +0800

    添加version启动标识

--- service_container/service.go
+++ service_container/service.go
@@ -22,7 +22,7 @@ import (
 var (
 	address = flag.String("address", ":50051", "服务器地址")
 )
-
+const version=1.0
 func main() {
 	flag.Parse()
 
@@ -31,7 +31,7 @@ func main() {
 		log.Fatalf("无法绑定地址: %v", err)
 	}
 
-	log.Println("server start at:",*address)
+	log.Println("server start at:",*address,"version",version)
 	s := grpc.NewServer()
 	pb.RegisterCrawlServer(s, &server{})
 	s.Serve(lis)
